be68902f3ce6b8b51b16fd298a8a27eec32d4c3e
Reza Moosavi <reza.moosavi@apsaaz.com>
2022-09-20 12:20:19 +0000

    Updated index.stg.js firebase

--- config/index.stg.js
+++ config/index.stg.js
@@ -17,12 +17,12 @@ const config = {
         'https://apsconnecthealingstorage.blob.core.windows.net/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2032-06-13T17:52:28Z&st=2022-06-13T09:52:28Z&spr=https&sig=JXb%2BKm9C3ZWDQmJ1SoS%2BT4nc78X9RP0Nw8eX%2BCfoLak%3D',
     containerName: 'images',
     firebase: {
-        apiKey: "AIzaSyA6S-D8ljGOtDr2tDScyaGE67w1vyVB7qw",
-        authDomain: "aps-connect-4healing-stg.firebaseapp.com",
-        projectId: "aps-connect-4healing-stg",
-        storageBucket: "aps-connect-4healing-stg.appspot.com",
-        messagingSenderId: "944770483240",
-        appId: "1:944770483240:web:cfb58b99070839ccde611d"
+        apiKey: "AIzaSyCiQMRaseeFONED5jLyWujIcSfekcVgJic",
+        authDomain: "aps-game-day-stg.firebaseapp.com",
+        projectId: "aps-game-day-stg",
+        storageBucket: "aps-game-day-stg.appspot.com",
+        messagingSenderId: "519966205749",
+        appId: "1:519966205749:web:acb18d8188808ebc968e7c"
     }
 };
 
