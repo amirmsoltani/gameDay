73068c4e26741b7930f3c2afb2a59c3e11a55a90
Reza Moosavi <reza.moosavi@apsaaz.com>
2022-10-18 09:26:39 +0000

    Updated index.prd.js

--- config/index.prd.js
+++ config/index.prd.js
@@ -1,28 +1,30 @@
 const apiUrl = 'https://aps-healthy-prd.azurewebsites.net';
 const baseUrl = 'https://witty-sea-0ae5f8e1e.azurestaticapps.net';
-const baseCononicalUrl = 'https://witty-sea-0ae5f8e1e.azurestaticapps.net';
-const subscriptionUrl = 'wss://aps-healthy-prd.azurewebsites.net/graphql';
-const supportMail = 'support@nubba.com';
-const generatorUrl = 'https://aps-genues-hotel-dev.azurewebsites.net/graphql/';
+const baseUrl = 'https://aps-game-day-prd.azurewebsites.net';
+const baseCononicalUrl = 'https://aps-game-day-prd.azurewebsites.net';
+const apiUrl = 'https://aps-game-day-prd.azurewebsites.net';
+const generatorUrl = 'https://aps-game-day-prd.azurewebsites.net/graphql/';
+const subscriptionUrl = 'wss://aps-game-day-prd.azurewebsites.net/graphql';
+const supportMail = 'fatemeh.karami@apsaaz.com';
 
 const config = {
     apiUrl,
     baseUrl,
+    generatorUrl,
     baseCononicalUrl,
     subscriptionUrl,
-    generatorUrl,
     supportMail,
-    blobBaseUrl: 'https://apshealthystorage.blob.core.windows.net/images',
+    blobBaseUrl: 'https://apsconnecthealingstorage.blob.core.windows.net/images',
     blobUrl:
-        'https://apshealthystorage.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2032-02-01T15:20:31Z&st=2022-02-01T07:20:31Z&spr=https&sig=7NKKvTpHCROh5RshoAKiGHlXgJv5%2FpI3CmLAsAlW3tw%3D',
+        'https://apsconnecthealingstorage.blob.core.windows.net/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2032-06-13T17:52:28Z&st=2022-06-13T09:52:28Z&spr=https&sig=JXb%2BKm9C3ZWDQmJ1SoS%2BT4nc78X9RP0Nw8eX%2BCfoLak%3D',
     containerName: 'images',
     firebase: {
-        apiKey: 'AIzaSyD5ACiY63e-NrL4qBB2-NywAmuJWJpFRLg',
-        authDomain: 'aps-healthy-prd.firebaseapp.com',
-        projectId: 'aps-healthy-prd',
-        storageBucket: 'aps-healthy-prd.appspot.com',
-        messagingSenderId: '298152592253',
-        appId: '1:298152592253:web:6c44e469d2610ccbe0c214'
+        apiKey: "AIzaSyBJSaOdM59d2QnzuHYq8IaojYmR8VW07a8",
+        authDomain: "aps-game-day-prd.firebaseapp.com",
+        projectId: "aps-game-day-prd",
+        storageBucket: "aps-game-day-prd.appspot.com",
+        messagingSenderId: "588566845999",
+        appId: "1:588566845999:web:0554c18549a17381f084da"
     }
 };
 
