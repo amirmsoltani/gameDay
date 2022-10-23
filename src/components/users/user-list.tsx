840b12c910b28c34a7861425b40db65c852bc95b
sam. khani <66492863+samakhani@users.noreply.github.com>
2022-10-23 16:19:53 +0330

    update users query

--- src/components/users/user-list.tsx
+++ src/components/users/user-list.tsx
@@ -1,10 +1,25 @@
 import { Divider, Grid } from '@mui/material';
-import React from 'react';
+import React, { FC } from 'react';
 import * as S from './users-style';
 import MoreMenu from './user-more-menu';
 import { MImage } from '../base/image/MImage';
 
-export const UserList = () => {
+type PropsType = {
+    active?: boolean;
+    onClick: () => void;
+    onChange: (checked: boolean) => void;
+    children?: undefined;
+    data: {
+        firstName?: string;
+        lastName?: string;
+        pictureUrl?: string;
+        phoneNumber?: string;
+        email?: string;
+        activeStatus?: boolean;
+    };
+};
+
+export const UserList: FC<PropsType> = ({ active, data, onClick }) => {
     return (
         <Grid>
             <S.ListBodyUser container gridRow={'span 1'}>
@@ -14,22 +29,22 @@ export const UserList = () => {
                         {/* <MImage resources={{ fallback: '/images/user.jpg' }} /> */}
                         <span>
                             Tolu Arowoselu
-                            {/* {data.firstName}
-                            {data.lastName} */}
+                            {data?.firstName}
+                            {data?.lastName}
                         </span>
                     </Grid>
                 </Grid>
                 <Grid lg={2.5} xs={12} className={'list-header__item'} item>
                     sampleemail@gmail.com
-                    {/* {data.email} */}
+                    {data?.email}
                 </Grid>
                 <Grid lg={3} xs={12} className={'list-header__item'} item>
                     +4498979883
-                    {/* {data?.phoneNumber} */}
+                    {data?.phoneNumber}
                 </Grid>
                 <Grid lg={2.5} xs={12} className={'list-header__item'} item>
                     Normal
-                    {/* {data.activeStatus} */}
+                    {data?.activeStatus}
                 </Grid>
                 <Grid lg={0.5} xs={12} className={'list-header__item'} item>
                     <MoreMenu />
